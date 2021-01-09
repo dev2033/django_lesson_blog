da104eb706792b68ab0aeb82cc35ff2786ef5efb
USER <dev,elop.02@mail.ru>
2021-01-09 19:07:38 +0300

    Добавил модели

--- blog/mainapp/models.py
+++ blog/mainapp/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    title = models.CharField('Категория', max_length=255)
+    slug = models.SlugField('Url' ,max_length=255, unique=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+
+class Tag(models.Model):
+
+    title = models.CharField('Тег', max_length=50)
+    slug = models.SlugField('Url', max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ['title']
+
+
+class Post(models.Model):
+
+    title = models.CharField('Название поста', max_length=255)
+    slug = models.SlugField('Url', max_length=255, unique=True)
+    author = models.CharField('Автор', max_length=100)
+    content = models.TextField('Контент', blank=True)
+    created_at = models.DateTimeField(
+        'Опубликовано',
+        auto_now_add=True
+    )
+    photo = models.ImageField('Изображение', upload_to='photos/%Y/%m/%d/', blank=True)
+    views = models.IntegerField('Колличество просмотров', default=0)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name='posts',
+        verbose_name='Категория'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True,
+        related_name='posts',
+        verbose_name='Теги'
+    )
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-created_at']

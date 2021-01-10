68b9f23ff64e32c097259719baaf0ac3b763e003
USER <dev,elop.02@mail.ru>
2021-01-10 21:29:00 +0300

    добавил вывод тегов, постов - сайдбар

--- blog/mainapp/views.py
+++ blog/mainapp/views.py
@@ -37,7 +37,18 @@ class PostsByCategory(ListView):
 
 class PostsByTag(ListView):
     """Выводит тэги"""
-    pass
+    template_name = 'mainapp/index.html'
+    context_object_name = 'posts'
+    paginate_by = 1
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Записи по тегу: ' + str(Tag.objects.get(slug=self.kwargs['slug']))
+        return context
 
 
 class GetPost(DetailView):
